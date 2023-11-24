129b9b9018b4a44117ad2a27d4d61d0d606f6767
iobaidu5 <i.obaidu5@gmail.com>
2023-11-24 10:37:19 +0500

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     var owl = $(".owl");
     owl.owlCarousel({
       items: 7,
-      navigation: true,
+      navigation: false,
       loop: true,
       rewind: true,
       autoplay: false,
@@ -22,8 +22,8 @@ $(document).ready(function () {
       // animateIn: "fadeIn",
       // animateOut: "fadeOut",
       navText: [
-        "<i class='fas fa-chevron-left nav-left'></i>",
-        "<i class='fas fa-chevron-right nav-right'></i>",
+        "",
+        "",
       ],
       responsiveClass: true,
       responsive: {
@@ -40,11 +40,11 @@ $(document).ready(function () {
           nav: true,
         },
         400: {
-          items: 1,
+          items: 2,
           nav: true,
         },
         768: {
-          items: 2,
+          items: 3,
           nav: true,
         },
         990: {
@@ -130,3 +130,113 @@ $(document).ready(function () {
       owl.trigger("stop.owl.autoplay");
     });
   });
+
+
+  $(function(){
+    $(".dropdown-menu > li > a.trigger").on("click",function(e){
+      var current=$(this).next();
+      var grandparent=$(this).parent().parent();
+      if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
+        $(this).toggleClass('right-caret left-caret');
+      grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+      grandparent.find(".sub-menu:visible").not(current).hide();
+      current.toggle();
+      e.stopPropagation();
+    });
+    $(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
+      var root=$(this).closest('.dropdown');
+      root.find('.left-caret').toggleClass('right-caret left-caret');
+      root.find('.sub-menu:visible').hide();
+    });
+  });
+
+
+  
+const galleryItem = document.getElementsByClassName("gallery-item");
+const lightBoxContainer = document.createElement("div");
+const lightBoxContent = document.createElement("div");
+const lightBoxImg = document.createElement("img");
+const lightBoxPrev = document.createElement("div");
+const lightBoxNext = document.createElement("div");
+
+lightBoxContainer.classList.add("lightbox");
+lightBoxContent.classList.add("lightbox-content");
+lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
+lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
+
+lightBoxContainer.appendChild(lightBoxContent);
+lightBoxContent.appendChild(lightBoxImg);
+lightBoxContent.appendChild(lightBoxPrev);
+lightBoxContent.appendChild(lightBoxNext);
+
+document.body.appendChild(lightBoxContainer);
+
+let index = 1;
+
+function showLightBox(n) {
+    if (n > galleryItem.length) {
+        index = 1;
+    } else if (n < 1) {
+        index = galleryItem.length;
+    }
+    let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
+    lightBoxImg.setAttribute("src", imageLocation);
+}
+
+function currentImage() {
+    lightBoxContainer.style.display = "block";
+
+    let imageIndex = parseInt(this.getAttribute("data-index"));
+    showLightBox(index = imageIndex);
+}
+for (let i = 0; i < galleryItem.length; i++) {
+    galleryItem[i].addEventListener("click", currentImage);
+}
+
+function slideImage(n) {
+    showLightBox(index += n);
+}
+function prevImage() {
+    slideImage(-1);
+}
+function nextImage() {
+    slideImage(1);
+}
+lightBoxPrev.addEventListener("click", prevImage);
+lightBoxNext.addEventListener("click", nextImage);
+
+function closeLightBox() {
+    if (this === event.target) {
+        lightBoxContainer.style.display = "none";
+    }
+}
+lightBoxContainer.addEventListener("click", closeLightBox);
+
+
+const nav = document.querySelector(".Navbar");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("navbar-active");
+  } else {
+    nav.classList.remove("navbar-active");
+  }
+}
+
+
+(function($){
+  $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass("show");
+    });
+
+    return false;
+  });
+})(jQuery)
